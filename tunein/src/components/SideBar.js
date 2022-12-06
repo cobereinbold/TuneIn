aad5aca24e30a0e134cc58403e9e6c5a6822a88b
Thomas Kahessay <Kahessaythomas@gmail.com>
2022-12-06 10:06:18 -0700

    Sidebar

---
+++ tunein/src/components/SideBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { MantineProvider, Text } from "@mantine/core";
+
+const SideBar = () => {
+  return (
+    <div>
+      <Text>String</Text>
+    </div>
+  );
+};
+
+export default SideBar;
