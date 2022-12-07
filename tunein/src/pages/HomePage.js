4190deae7cea61fcbcedc72afa29aa71a26d28a1
Thomas Kahessay <Kahessaythomas@gmail.com>
2022-12-07 16:43:44 -0700

    Added posts to home page

--- tunein/src/pages/HomePage.js
+++ tunein/src/pages/HomePage.js
@@ -1,6 +1,78 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import {
+  Group,
+  Center,
+  Container,
+  Stack,
+  AppShell,
+  Space,
+} from "@mantine/core";
 import SideBar from "../components/SideBar";
+import Post from "../components/Post";
+import songImage1 from "../images/midnights.jpeg";
+import songImage2 from "../images/herloss.jpeg";
+
+const posts = [
+  {
+    user: {
+      id: "1",
+      userName: "Benjamin",
+      profilePicture: "",
+    },
+    songInfo: {
+      song: "Anti-Hero",
+      artist: "Taylor Swift",
+      spotifyLink:
+        "https://open.spotify.com/track/0V3wPSX9ygBnCm8psDIegu?si=f8bdfd471fb44fdf",
+      songImage: songImage1,
+    },
+    likes: {
+      count: 12,
+      users: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    },
+    caption: "LOVING THIS NEW ALBUM!",
+    comments: [
+      {
+        userName: "billy",
+        comment: "cool",
+      },
+      {
+        userName: "thomas",
+        comment: "I hate this song.",
+      },
+    ],
+  },
+  {
+    user: {
+      id: "2",
+      userName: "Timothy",
+      profilePicture: "",
+    },
+    songInfo: {
+      song: "Rich Flex",
+      artist: "Drake ft. 21 Savage",
+      spotifyLink:
+        "https://open.spotify.com/track/1bDbXMyjaUIooNwFE9wn0N?si=b68ca04005034725",
+      songImage: songImage2,
+    },
+    likes: {
+      count: 5,
+      users: [6, 7, 8, 9, 10],
+    },
+    caption: "21 can you do sum for me?",
+    comments: [
+      {
+        userName: "billy",
+        comment: "cool",
+      },
+      {
+        userName: "thomas",
+        comment: "I hate this song.",
+      },
+    ],
+  },
+];
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -17,9 +89,13 @@ const HomePage = () => {
   const retrievePosts = () => {};
 
   return (
-    <>
-      <SideBar activePage='HOME' />
-    </>
+    <AppShell navbar={<SideBar activePage='HOME' />}>
+      <Stack justify='flex-start'>
+        {posts.map((post) => {
+          return <Post {...post} />;
+        })}
+      </Stack>
+    </AppShell>
   );
 };
 
