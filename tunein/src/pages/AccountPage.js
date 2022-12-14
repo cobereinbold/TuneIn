44aa7a726964e6e3ed52bd44690b54b30d55c689
Thomas Kahessay <Kahessaythomas@gmail.com>
2022-12-14 16:30:43 -0700

    Fixed account bug

--- tunein/src/pages/AccountPage.js
+++ tunein/src/pages/AccountPage.js
@@ -196,7 +196,7 @@ const AccountPage = () => {
   };
 
   return (
-    <AppShell navbar={<SideBar activePage="ACCOUNT" />}>
+    <AppShell navbar={<SideBar activePage='ACCOUNT' />}>
       <Center>
         <Image
           src={
@@ -206,33 +206,33 @@ const AccountPage = () => {
           }
           width={200}
           height={200}
-          radius="100%"
+          radius='100%'
           withPlaceholder
         />
       </Center>
-      <Space h="lg" />
+      <Space h='lg' />
       <Center>
         <Stack>
           <Button onClick={() => setEditModalOpen(true)}>Edit Profile</Button>
-          <Title ta="center" order={1}>
+          <Title ta='center' order={1}>
             {"@" + user.username}
           </Title>
-          <Title ta="center" order={2}>
+          <Title ta='center' order={2}>
             {user.firstName + " " + user.lastName}
           </Title>
-          <Title ta="center" order={3}>
+          <Title ta='center' order={3}>
             Favourite Genre: {user.favoriteGenre}
           </Title>
-          <Title ta="center" order={3}>
+          <Title ta='center' order={3}>
             Joined: {user.dateJoined}
           </Title>
         </Stack>
       </Center>
-      <Space h="lg" />
+      <Space h='lg' />
       <Center>
         <SimpleGrid
           cols={4}
-          spacing="xl"
+          spacing='xl'
           breakpoints={[
             { maxWidth: 1800, cols: 4, spacing: "xl" },
             { maxWidth: 1500, cols: 3, spacing: "xl" },
@@ -250,7 +250,7 @@ const AccountPage = () => {
                   setModalOpen(true);
                   setSelectedPost(post);
                 }}
-                className="image"
+                className='image'
               />
             );
           })}
@@ -267,23 +267,24 @@ const AccountPage = () => {
             src={selectedPost.songInfo.songImage}
             height={400}
             width={400}
-            onClick={() =>
-              window.open(selectedPost.songInfo.spotifyLink, "_black")
-            }
+            onClick={() => {
+              window.open(selectedPost.songInfo.songLink, "_blank");
+              console.log(selectedPost);
+            }}
           ></Image>
         </Center>
-        <Space h="md" />
-        <Title order={3} ta="center">
+        <Space h='md' />
+        <Title order={3} ta='center'>
           {selectedPost.songInfo.song + ": " + selectedPost.songInfo.artist}
         </Title>
-        <Text fz="md" ta="center" color="spGreen">
+        <Text fz='md' ta='center' color='spGreen'>
           {selectedPost.caption}
         </Text>
-        <Group spacing="none" position="center">
-          <IconHeart size={20} fill={"red"} color="red" />
+        <Group spacing='none' position='center'>
+          <IconHeart size={20} fill={"red"} color='red' />
           <Text>{selectedPost.likes.count + " Likes"}</Text>
-          <Space w="lg" />
-          <IconDots size={20} color="white" />
+          <Space w='lg' />
+          <IconDots size={20} color='white' />
           <Text>{selectedPost.comments.length + " Comments"}</Text>
         </Group>
       </Modal>
@@ -291,62 +292,62 @@ const AccountPage = () => {
         centered
         opened={editModalOpen}
         onClose={() => setEditModalOpen(false)}
-        title="Edit Account"
+        title='Edit Account'
       >
-        <Box sx={{ maxWidth: 400 }} mx="auto">
+        <Box sx={{ maxWidth: 400 }} mx='auto'>
           <form
             onSubmit={accountForm.onSubmit((values) => updateAccount(values))}
           >
             <TextInput
               withAsterisk
-              label="Email"
-              placeholder="you@gmail.com"
+              label='Email'
+              placeholder='you@gmail.com'
               {...accountForm.getInputProps("email")}
             ></TextInput>
-            <Space h="lg" />
+            <Space h='lg' />
             <TextInput
               withAsterisk
-              label="Username"
-              placeholder="JohnSmith"
+              label='Username'
+              placeholder='JohnSmith'
               {...accountForm.getInputProps("username")}
             ></TextInput>
-            <Space h="lg" />
+            <Space h='lg' />
             <TextInput
               withAsterisk
-              label="First Name"
-              placeholder="John"
+              label='First Name'
+              placeholder='John'
               {...accountForm.getInputProps("firstName")}
             ></TextInput>
-            <Space h="lg" />
+            <Space h='lg' />
             <TextInput
               withAsterisk
-              label="Last Name"
-              placeholder="Smith"
+              label='Last Name'
+              placeholder='Smith'
               {...accountForm.getInputProps("lastName")}
             ></TextInput>
-            <Space h="lg" />
+            <Space h='lg' />
             <PasswordInput
               withAsterisk
-              label="Password"
-              placeholder="Password"
+              label='Password'
+              placeholder='Password'
               {...accountForm.getInputProps("password")}
             />
-            <Space h="lg" />
+            <Space h='lg' />
             <FileInput
-              label="Profile Picture"
-              placeholder="Profile Picture"
+              label='Profile Picture'
+              placeholder='Profile Picture'
               icon={<IconUpload size={14} />}
-              accept="image/png,image/jpeg"
+              accept='image/png,image/jpeg'
               value={file}
               onChange={setFile}
             />
-            <Space h="sm" />
-            <Text fz="xs" ta="center" color="red">
+            <Space h='sm' />
+            <Text fz='xs' ta='center' color='red'>
               Warning: You must log back in after updating your profile.
             </Text>
-            <Space h="sm" />
+            <Space h='sm' />
             <Center>
-              <Button type="submit">Update Info</Button>
+              <Button type='submit'>Update Info</Button>
             </Center>
           </form>
         </Box>
